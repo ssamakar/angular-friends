45c8e3a37d0344bc1147fe734d201d7f0b3e47a3
Saba Samakar <saba.samakar@gmail.com>
2014-04-15 20:46:10 -0600

    Done through step 4 of readme

--- FriendController.js
+++ FriendController.js
@@ -1,5 +1,13 @@
-
-
-
-
-
+var friendController = function($scope, $http){
+	$scope.test = "Hello World";
+	// $http({method: 'GET', url: "https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json"}).
+	// 	success(function(data){
+	// 		$scope.friends = data.results;
+	// 	}).
+	// 	error(function(){
+	// 		return "http GET failed";
+	// 	});
+	$http.get("https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json").success(function(data){
+			$scope.friends = data.results;
+		});
+}
---
+++ app.js
@@ -0,0 +1,8 @@
+(function(){
+	'use strict';
+	var app = angular.module("angular-friends",[]); 
+	//empty array arg lets us know this module isn't dependent on any other
+
+
+
+})();
