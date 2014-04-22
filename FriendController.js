4a1efd58a062e6b626f8d5266bc565b766cc8d7d
Saba Samakar <saba.samakar@gmail.com>
2014-04-22 15:20:59 -0600

    Finished exercises

--- FriendController.js
+++ FriendController.js
@@ -1,13 +1,23 @@
-var friendController = function($scope, $http){
+angular.module('angular-friends').service('friendService', function($http){
+	return {
+		getFriends:function(){
+			return 	$http.get("https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json")
+		}
+	}
+})
+
+angular.module('angular-friends').controller('friendController', function($scope, friendService){
 	$scope.test = "Hello World";
-	// $http({method: 'GET', url: "https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json"}).
-	// 	success(function(data){
-	// 		$scope.friends = data.results;
-	// 	}).
-	// 	error(function(){
-	// 		return "http GET failed";
-	// 	});
-	$http.get("https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json").success(function(data){
+
+	friendService.getFriends().success(function(data){
 			$scope.friends = data.results;
 		});
-}
+
+	$scope.alphabetSort = "false";
+	$scope.sortOptions = [{val: "name", string: "Name"}, 
+		{val: "friend_count", string: "Number of Friends"}, 
+		{val: "current_location.name", string: "City"}, 
+		{val: "current_location.country", string: "Country"}
+		]; 
+
+});
